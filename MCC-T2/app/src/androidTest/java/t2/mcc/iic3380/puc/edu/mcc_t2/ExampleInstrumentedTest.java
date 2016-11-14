b75465fb8dfa424c893294788710adf818867e18
quelves <luiz.quelves@gmail.com>
2016-11-14 20:13:51 -0300

    first version

---
+++ MCC-T2/app/src/androidTest/java/t2/mcc/iic3380/puc/edu/mcc_t2/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package t2.mcc.iic3380.puc.edu.mcc_t2;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("t2.mcc.iic3380.puc.edu.mcc_t2", appContext.getPackageName());
+    }
+}
