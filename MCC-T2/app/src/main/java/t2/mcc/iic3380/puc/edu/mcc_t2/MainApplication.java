b7f17868d985f2a6d460ed749714655f290eae30
quelves <luiz.quelves@gmail.com>
2016-11-15 16:49:38 -0300

    Changes for suporte multidex

--- MCC-T2/app/src/main/java/t2/mcc/iic3380/puc/edu/mcc_t2/MainApplication.java
+++ MCC-T2/app/src/main/java/t2/mcc/iic3380/puc/edu/mcc_t2/MainApplication.java
@@ -1,6 +1,9 @@
 package t2.mcc.iic3380.puc.edu.mcc_t2;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import edu.puc.astral.CloudManager;
 
@@ -8,7 +11,7 @@ import edu.puc.astral.CloudManager;
  * Created by quelves on 11/15/16.
  */
 
-public class MainApplication extends Application {
+public class MainApplication extends MultiDexApplication {
     private static final String HOST_IP = "http://35.163.228.128";
     private static final String SENDER_ID = "460750150714";
 
@@ -18,6 +21,13 @@ public class MainApplication extends Application {
 
         CloudManager.initialize(this, SENDER_ID, HOST_IP);
     }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(MainApplication.this);
+    }
+
 }
 
 
